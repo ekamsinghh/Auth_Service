110ae6bf252eddcfc6fed920c2cb564f5c4ea3eb
Ekam Singh <ekamsinghh24@gmail.com>
2025-05-23 16:25:26 +0530

    Added function for checking the incoming password with the encrypted password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
 const UserRepository= require('../repository/user-repository');
+const jwt= require('jsonwebtoken');
+const {JWT_KEY}= require('../config/serverConfig');
+const bcrypt= require('bcrypt');
 
 class UserService{
     constructor(){
@@ -23,6 +26,39 @@ class UserService{
         }
         catch(error){
             console.log("Some error occured in service layer");
+            throw {error};
+        }
+    }
+
+    createToken(user) {
+        try{
+            const response= jwt.sign(user,JWT_KEY,{
+                expiresIn:'1h'
+            })
+            return response;
+        }
+        catch(error){
+            console.log("Something went wrong in token creation");
+        }
+    }
+
+    verifyToken(token){
+        try{
+            const response= jwt.verify(token,JWT_KEY);
+            return response;
+        }
+        catch(error){
+            console.log("Something went wrong in token verification",error);
+            throw {error};
+        }
+    }
+
+    checkPassword(userInputPlainPassword,encryptedPassword){
+        try{
+            return bcrypt.compare(userInputPlainPassword,encryptedPassword);
+        }
+        catch(error){
+            console.log("Something Went wrong in Password checking");
             throw {error};
         }
     }
