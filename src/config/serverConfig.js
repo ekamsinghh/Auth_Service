c279f53465ed559fef227179a58fc0bcd1156bda
Ekam Singh <ekamsinghh24@gmail.com>
2025-05-23 21:31:30 +0530

    Added roles and user_roles through tables

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,5 +5,5 @@ dotenv.config();
 module.exports={
     PORT:process.env.PORT,
     SALT: bcryprt.genSaltSync(10),
-    JWT_KEY:process.env.JWT_KEY
+    JWT_KEY:process.env.JWT_KEY,
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const {PORT}= require('./config/serverConfig');
 const ApiRoutes=require("./routes/index");
-const UserService= require('./services/user-service');
+const sequelize= require('sequelize')
+const db= require('./models/index');
+const {User,Role}= require('./models/index');
+
 const setupAndStartServer= async ()=>{
     const app= express();
     
@@ -12,9 +15,15 @@ const setupAndStartServer= async ()=>{
 
     app.listen(PORT,async ()=>{
         console.log(`Server is running on ${PORT}`);
-        // const userService= new UserService();
-        // const result=await userService.signIn("xyz@abc.com","waheguru13");
-        // console.log(result);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+
+        // const u1= await User.findByPk(2);
+        // const r1=await Role.findByPk(1);
+        // u1.addRole(r1);
+        // const response= await u1.hasRole(r1);
+        // console.log(response);
     });
 };
 
